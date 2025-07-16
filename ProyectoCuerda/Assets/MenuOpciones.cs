ccb481b4f0b0b68c37a46324ac0589e3dcaf4d2a
aniokku <>
2025-07-16 10:49:26 -0500

    menu opciones

---
+++ ProyectoCuerda/Assets/MenuOpciones.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MenuOpciones : MonoBehaviour
+{
+
+    [SerializeField] private AudioMixer volumeMixer;
+    [SerializeField] private AudioMixer efectosMixer;
+
+    public void PantallaCompleta(bool pantallaCompleta)
+    {
+        
+        Screen.fullScreen = pantallaCompleta;
+    }
+
+    public void CambiarVolumen(float volumen)
+    {
+        volumeMixer.SetFloat("Musica", volumen);
+    }
+    
+    public void CambiarEfectos(float volumen)
+    {
+
+        efectosMixer.SetFloat("Efectos", volumen);
+    }
+}
